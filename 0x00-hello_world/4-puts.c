2d9421dfd6fc1764c90c99462c3d7bd1695916b7
dani198 <danyleul16@gmail.com>
2022-06-30 11:08:06 -0700

    trying

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
 /*
- *
- * * main - Entry Point
- *
- * *
- *
- * * Return: Always 0(Success)
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- */
+ * main - Entry Point
+ * Return: Always 0 (Success)
+  */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
