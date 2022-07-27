7b1dc31b4cb3346e9cc48fd5efd300eafe9a358f
dani198 <danyleul16@gmail.com>
2022-07-27 11:11:16 -0700

    concatnate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,10 +3,11 @@
 #include <stdlib.h>
 /**
  * str_concat - concatenate strings
- * @s1: string 1
  *
+ *  @s1: string 1
  * @s2: string 2
- * Return: NULL if failure
+ *
+ *  Return: NULL if failure
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -19,7 +20,7 @@ s2 = "";
 for (a = 0; s1[a] != '\0'; a++);
 for (b = 0; s2[b] != '\0'; b++);
 b++;
-s = malloc(a * sizeof(*s1) + b * sizeof(*s2));
+s = malloc(a *sizeof(*s1) + b *sizeof(*s2));
 if (s == 0)
 return (NULL);
 for (c = 0, d = 0; c < a + b; c++)
